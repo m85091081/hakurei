aea2a9c2c857cd13d6b7f8d968c7d0481bed325c
m85091081 <b10216041@yuntech.edu.tw>
2016-03-05 22:57:30 +0800

    fix site

--- muMDAU_app/index.py
+++ muMDAU_app/index.py
@@ -2,7 +2,6 @@
 # muMDAU_app main / first page 
 from muMDAU_app import app 
 from flask import request, render_template, Blueprint, url_for, redirect
-from database import countUSER, ManageSQL, LoginSQL
 
 main = Blueprint('main', __name__)
 
@@ -14,32 +13,10 @@ def index():
     else:
         return render_template('index.html')
 
-# init route to first time use
-@app.route('/init', methods=['GET', 'POST'])
-def init():
+@main.route('/blog', methods=['GET', 'POST'])
+def blog():
     if request.method == 'POST':
-        user = request.form['buser']
-        passd = request.form['bpass']
-        import hashlib
-        hashsha = hashlib.sha256(passd.replace('\n', '').encode())
-        ManageSQL.addUser(user, hashsha.hexdigest(), '1', '0')
-        return redirect(url_for('main.index'))
+        pass
     else:
-        return render_template('first.html')
+        return render_template('blog.html')
 
-# test of adduser route page 
-@app.route('/adduser', methods=['GET', 'POST'])
-def adduser():
-    if request.method == 'POST':
-        user = request.form['buser']
-        if LoginSQL.getPass(user) is None:
-            import hashlib
-            import random
-            ans = random.uniform(1, 10)
-            hashpass1 = hashlib.sha1(str(ans).encode())
-            passd1 = hashpass1.hexdigest()
-            hashpass0 = hashlib.sha256(passd1.replace('\n', '').encode())
-            ManageSQL.addUser(user, hashpass0.hexdigest(), '0', '1')
-            return passd1
-        else:
-            return '使用者已經他媽的存在了喔！'
