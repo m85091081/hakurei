4c14c2e244cb05ddddfd67fff85203b0cfe30a49
m85091081 <b10216041@yuntech.edu.tw>
2016-03-05 16:26:37 +0800

    new hakurei site

--- muMDAU_app/__init__.py
+++ muMDAU_app/__init__.py
@@ -1,21 +1,30 @@
 # -*- coding: utf-8 -*-
-
+# muMDAU_app init file 
+# some debug code of server like update/restart code
 from flask import Flask, request, session, redirect, url_for
 from flask import render_template 
 
 app = Flask(__name__)
-import subprocess
 
+import subprocess
+import muMDAU_app.login 
+import muMDAU_app.logout 
+import muMDAU_app.panel
+
+# private function - runtime error to catch restart
 def restart_server():
     func = request.environ.get('werkzeug.server.shutdown')
     if func is None:
         raise RuntimeError('Not running with the Werkzeug Server')
     func()
+# private function - subprocess run git pull
 def gitpull():
     subprocess.call(['git pull'], shell=True)
+# private function - debug only pkill pypy3
 def pkill_server():
     subprocess.call(['pkill pypy3'], shell=True)
 
+# muMDAU_app: do server update and restart
 @app.route('/update')
 def update():
     if 'username' in session:
@@ -25,6 +34,7 @@ def update():
     else:
         return redirect(url_for('loginp'))
 
+# muMDAU_app: do restart without update
 @app.route('/restart')
 def restart():
     if 'username' in session:
@@ -33,3 +43,14 @@ def restart():
     else:
         return redirect(url_for('loginp'))
 
+# muMDAU_app: **debug** shutdown_server
+@app.route('/shutdown')
+def shutdown_server():
+    if muMDAU_app.setting.debug == 1:
+        if 'username' in session:
+            pkill_server()
+            return 'server go to shutdown'
+        else:
+            return redirect(url_for('loginp'))
+    else:
+        return 'debug mode is disable , please use restart function'
--- muMDAU_app/index.py
+++ muMDAU_app/index.py
@@ -1,20 +1,50 @@
 # -*- coding: utf-8 -*-
-
-from flask import request, render_template, Blueprint
+# muMDAU_app main / first page 
+from muMDAU_app import app 
+from flask import request, render_template, Blueprint, url_for, redirect
+from database import countUSER, ManageSQL, LoginSQL
 
 main = Blueprint('main', __name__)
 
+# index page main route page 
 @main.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         pass
     else:
-        return render_template('index.html')
+        # check admin is visable ?
+        answer = countUSER.countAdmin()
+        if answer[0] == 0:
+            return redirect(url_for('init'))
+        else:
+            return render_template('index.html')
 
-
-@main.route('/about', methods=['GET', 'POST'])
-def about():
+# init route to first time use
+@app.route('/init', methods=['GET', 'POST'])
+def init():
     if request.method == 'POST':
-        pass
+        user = request.form['buser']
+        passd = request.form['bpass']
+        import hashlib
+        hashsha = hashlib.sha256(passd.replace('\n', '').encode())
+        ManageSQL.addUser(user, hashsha.hexdigest(), '1', '0')
+        return redirect(url_for('main.index'))
     else:
-        return render_template('about.html')
+        return render_template('first.html')
+
+# test of adduser route page 
+@app.route('/adduser', methods=['GET', 'POST'])
+def adduser():
+    if request.method == 'POST':
+        user = request.form['buser']
+        if LoginSQL.getPass(user) is None:
+            import hashlib
+            import random
+            ans = random.uniform(1, 10)
+            hashpass1 = hashlib.sha1(str(ans).encode())
+            passd1 = hashpass1.hexdigest()
+            hashpass0 = hashlib.sha256(passd1.replace('\n', '').encode())
+            ManageSQL.addUser(user, hashpass0.hexdigest(), '0', '1')
+            return passd1
+        else:
+            return '使用者已經他媽的存在了喔！'
