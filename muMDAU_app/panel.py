a6c5871443f90101dd17150d345c0c95dd976403
m85091081 <b10216041@yuntech.edu.tw>
2016-03-07 16:28:53 +0800

    add panel of event

--- muMDAU_app/panel.py
+++ muMDAU_app/panel.py
@@ -2,12 +2,51 @@
 
 from muMDAU_app import app
 from flask import request, session, render_template, url_for, redirect
+from database import ManageSQL, countUSER, EventSQL
 import setting
 import os
+
+@app.route('/event/add', methods=['GET', 'POST'])
+def eadd():
+    if request.method == 'POST':
+        if 'username' in session:
+            name = request.form['name']
+            date = request.form['date']
+            EventSQL.addEvent(name, date)
+            return redirect(url_for('panel'))
+        else:
+            return 'nologin'
+
+@app.route('/event/del/<ev>', methods=['GET', 'POST'])
+def edel(ev):
+    if request.method == 'GET':
+        if 'username' in session:
+            EventSQL.delEvent(ev)
+            return redirect(url_for('panel'))
+        else:
+            return redirect(url_for('panel'))
+
+@app.route('/init', methods=['GET', 'POST'])
+def init():
+    if request.method == 'POST':
+        user = request.form['buser']
+        passd = request.form['bpass']
+        import hashlib
+        hashsha = hashlib.sha256(passd.replace('\n', '').encode())
+        ManageSQL.addUser(user, hashsha.hexdigest(), '1', '0')
+        return redirect(url_for('panel'))
+    else:
+        answer = countUSER.countAdmin()
+        if answer[0] == 0:
+            return render_template('first.html')
+        else:
+            return redirect(url_for('panel'))
+
 @app.route('/panel')
 def panel():
     if 'username' in session:
-        return render_template('panel.html', username=session['username'])
+        elist = EventSQL.listAllEvent()
+        return render_template('edit.html', **locals())
     else:
         return redirect(url_for('loginp'))
 
