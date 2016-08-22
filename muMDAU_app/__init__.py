02dced30a09d23ab89e2c7b5837c72aff022d950
m85091081 <b10216041@yuntech.edu.tw>
2016-08-22 22:10:06 +0800

    fix admin panel

--- muMDAU_app/__init__.py
+++ muMDAU_app/__init__.py
@@ -2,8 +2,5 @@
 # muMDAU_app init file 
 # some debug code of server like update/restart code
 from flask import Flask , render_template
-from flask_wtf.csrf import CsrfProtect
-
 app = Flask(__name__)
-csrf = CsrfProtect(app)
 
