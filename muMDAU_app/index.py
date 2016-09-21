de87fd20ba17488cb243fadcf5dc3954685e9edc
m85091081 <b10216041@yuntech.edu.tw>
2016-09-21 19:46:57 +0800

    fix index range of null

--- muMDAU_app/index.py
+++ muMDAU_app/index.py
@@ -9,10 +9,14 @@ main = Blueprint('main', __name__)
 @main.route('/')
 def index():
     elist = EventSQL.listEvent(3)
-    E1Name = elist[0][0]  # NOQA
-    E2Name = elist[1][0]  # NOQA
-    E3Name = elist[2][0]  # NOQA
-    E1D = elist[0][1]  # NOQA
-    E2D = elist[1][1]  # NOQA
-    E3D = elist[2][1]  # NOQA
+    if elist == [] :
+        E1Name = '尚未決定'  # NOQA
+        E1D = '尚未決定'  # NOQA
+    if not elist == []:
+        E1Name = elist[0][0]  # NOQA
+        E2Name = elist[1][0]  # NOQA
+        E3Name = elist[2][0]  # NOQA
+        E1D = elist[0][1]  # NOQA
+        E2D = elist[1][1]  # NOQA
+        E3D = elist[2][1]  # NOQA
     return render_template('index.html', **locals())
